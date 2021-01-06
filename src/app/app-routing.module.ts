781ddc32fd085c972da7941a21b5aa50c747add6
Ángel Quiroz <guirudj007@gmail.com>
2021-01-06 00:47:57 -0500

    Rutas y Componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { EmpresaComponent } from "./components/empresa/empresa.component";
+import { EmpresasComponent } from "./components/empresas/empresas.component";
+import { HomeComponent } from "./components/home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: HomeComponent },
+  { path: "empresas", component: EmpresasComponent },
+  { path: "empresas/:nombre", component: EmpresaComponent },
+  { path: "**", redirectTo: "" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { EmpresasComponent } from './components/empresas/empresas.component';
+import { EmpresaComponent } from './components/empresa/empresa.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EmpresasComponent,
+    EmpresaComponent
   ],
   imports: [
     BrowserModule,
